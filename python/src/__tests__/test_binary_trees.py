05a3671ba639064a926dede3f44d8340549b1f04
eXzacT <damjan.andelkovic1@gmail.com>
2024-02-26 20:42:03 +0100

    day 28

--- python/src/__tests__/test_binary_trees.py
+++ python/src/__tests__/test_binary_trees.py
@@ -75,3 +75,59 @@ def test_is_same(test_input, expected):
 
     for func in [is_same_dfs, is_same_iter, is_same_bfs]:
         assert func(tree1, tree2) == expected
+
+
+test_cases = [
+    (([3, 4, 5, 1, 2], [4, 1, 2]), True),
+    (([3, 4, 5, 1, 2, None, None, None, None, 0], [4, 1, 2]), False),
+    (([1, 1], [1]), True),
+    (([3, 4, 5, 1, None, 2], [3, 1, 2]), False),
+    (([4, 1, None, 1, None, 6, 7], [4, 1, None, 6, 7]), False),
+    (([1, None, 1, None, 1, None, 1, None, 1, None, 1, None, 1, None, 1, None, 1,
+     None, 1, None, 1, 2], [1, None, 1, None, 1, None, 1, None, 1, None, 1, 2]), True),
+]
+
+
+@pytest.mark.parametrize("test_input,expected", test_cases)
+def test_is_subtree(test_input, expected):
+    print(f"\nFor input {test_input}")
+    l1, l2 = test_input
+    tree1, tree2 = create(l1), create(l2)
+
+    for func in [is_subtree, is_subtree_v2]:
+        assert func(tree1, tree2) == expected
+
+
+test_cases = [
+    (([6, 2, 8, 0, 4, 7, 9, None, None, 3, 5], 2, 8), 6),
+    (([6, 2, 8, 0, 4, 7, 9, None, None, 3, 5], 2, 4), 2),
+    (([2, 1], 2, 1), 2),
+    (([28, 12, 45, 4, 24, 35, 47, 2, 9, 14, 25, 31, 42, 46, 48, 0, 3, 8, 11, 13, 20, None, 26, 30, 33, 41, 43, None, None, None, 49, None, 1, None, None, 7, None, 10, None, None, None, 17, 22, None, 27, 29, None, 32, 34, 36, None, None, 44, None, None,
+     None, None, 6, None, None, None, 16, 18, 21, 23, None, None, None, None, None, None, None, None, None, 37, None, None, 5, None, 15, None, None, 19, None, None, None, None, None, 40, None, None, None, None, None, None, 39, None, 38], 1, 23), 12),
+]
+
+
+@pytest.mark.parametrize("test_input,expected", test_cases)
+def test_lowest_common_ancestor(test_input, expected):
+    print(f"\nFor input {test_input}")
+    l, p, q = test_input
+    tree = create(l)
+    p, q = TreeNode(p), TreeNode(q)
+
+    for func in [lowest_common_ancestor, lowest_common_ancestor_v2, lowest_common_ancestor_v3]:
+        assert func(tree, p, q).val == expected
+
+
+test_cases = [
+    ([3, 9, 20, None, None, 15, 7], [[3], [9, 20], [15, 7]]),
+    ([1], [[1]]),
+    ([], []),
+]
+
+
+@pytest.mark.parametrize("test_input,expected", test_cases)
+def test_lowest_common_ancestor(test_input, expected):
+    print(f"\nFor input {test_input}")
+
+    for func in [level_order_traversal_iter, level_order_traversal_rec]:
+        assert func(create(test_input)) == expected
--- python/src/binary_trees.py
+++ python/src/binary_trees.py
@@ -48,6 +48,36 @@ def display(head: TreeNode) -> list:
     return bfs
 
 
+def print_tree(root):
+    def tree_depth(node):
+        if not node:
+            return 0
+        left_depth = tree_depth(node.left)
+        right_depth = tree_depth(node.right)
+        return max(left_depth, right_depth) + 1
+
+    def fill_positions(grid, node, depth, pos, offsets):
+        if depth + 1 < len(grid):
+            if node:
+                grid[depth][pos] = str(node.val)
+                fill_positions(grid, node.left, depth + 1,
+                               pos - offsets[depth + 1], offsets)
+                fill_positions(grid, node.right, depth + 1,
+                               pos + offsets[depth + 1], offsets)
+            else:
+                grid[depth][pos] = 'x'
+
+    depth = tree_depth(root)
+    width = 2 ** depth - 1
+    grid = [[" " for _ in range(width)] for _ in range(depth)]
+
+    offsets = [2 ** (depth - i - 1) for i in range(depth)]
+    fill_positions(grid, root, 0, width // 2, offsets)
+
+    tree_str = "\n".join("".join(row) for row in grid)
+    return tree_str
+
+
 '''Given the root of a binary tree, invert the tree, and return its root.'''
 
 
@@ -225,7 +255,7 @@ def is_balanced_dfs_v2(root: TreeNode) -> bool:
     return is_balanced
 
 
-''' Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+''' Given the roots of two binary trees s and t, write a function to check if they are the same or not.
     Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.'''
 
 
@@ -278,3 +308,180 @@ def is_same_bfs(root1: TreeNode, root2: TreeNode) -> bool:
             return False
 
     return True
+
+
+''' Given the roots of two binary trees root and subroot, return true if there is a subtree of root with the same structure 
+    and node values of subRoot and false otherwise.
+    A subtree of a binary tree tree is a tree that consists of a node in tree and all of this node's descendants. 
+    The tree tree could also be considered as a subtree of itself.'''
+
+
+@time_execution()
+def is_subtree(s: TreeNode, t: TreeNode) -> bool:
+    def is_same(s: TreeNode, t: TreeNode) -> bool:
+        if not s or not t:  # Make sure they're both None if one of them is None
+            return s == t
+
+        return s.val == t.val and is_same(s.left, t.left) and is_same(s.right, t.right)
+
+    # For every node in 's' check whether the tree below including it is same as tree 't'
+    stack = [s]
+    while stack:
+        curr = stack.pop()
+        if is_same(curr, t):
+            return True
+        else:
+            if curr.left:
+                stack.append(curr.left)
+            if curr.right:
+                stack.append(curr.right)
+
+    return False
+
+
+@time_execution()
+def is_subtree_v2(s: TreeNode, t: TreeNode) -> bool:
+    def is_same(s: TreeNode, t: TreeNode) -> bool:
+        if not s or not t:  # Make sure they're both None if one of them is None
+            return s == t
+
+        return s.val == t.val and is_same(s.left, t.left) and is_same(s.right, t.right)
+
+    def helper(s: TreeNode, t: TreeNode) -> bool:
+        if not t:
+            return True
+        if not s:
+            return False
+
+        return is_same(s, t) or helper(s.left, t) or helper(s.right, t)
+
+    return helper(s, t)
+
+
+''' Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
+    The lowest common ancestor is defined between two nodes p and q as the lowest node in T 
+    that has both p and q as descendants (where we allow a node to be a descendant of itself).'''
+
+
+@time_execution()
+def lowest_common_ancestor(root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+    index_map = {0: root}  # Position: Node
+    i = j = None
+
+    def helper(node: TreeNode, idx: int):
+        nonlocal i, j
+        if not node:
+            return
+
+        if node.val == p.val:
+            i = idx
+        elif node.val == q.val:
+            j = idx
+
+        index_map[2*idx+1] = node.left
+        index_map[2*idx+2] = node.right
+        helper(node.left, 2*idx+1)
+        helper(node.right, 2*idx+2)
+
+    helper(root, 0)
+
+    # Find the index of LCA by moving up from the child which is deeper until we're at the same position (same ancestor)
+    while i != j:
+        if i > j:
+            i = (i - 1) // 2
+        else:
+            j = (j - 1) // 2
+
+    return index_map[i]
+
+
+@time_execution()
+def lowest_common_ancestor_v2(root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+    index_map = {0: root}  # Position: Node
+    i = j = None
+    queue = deque([(root, 0)])
+    while queue:
+        curr, idx = queue.popleft()
+        if not curr:
+            continue
+
+        if curr.val == p.val:
+            i = idx
+        elif curr.val == q.val:
+            j = idx
+
+        if i != None and j != None:
+            break
+
+        index_map[2*idx+1] = curr.left
+        index_map[2*idx+2] = curr.right
+        queue.append((curr.left, 2*idx+1))
+        queue.append((curr.right, 2*idx+2))
+
+    # Find the index of LCA by moving up from the child which is deeper until we're at the same position (same ancestor)
+    while i != j:
+        if i > j:
+            i = (i - 1) // 2
+        else:
+            j = (j - 1) // 2
+
+    return index_map[i]
+
+
+@time_execution()
+def lowest_common_ancestor_v3(root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+    def helper(curr: TreeNode) -> TreeNode:
+        if curr.val < p.val and curr.val < q.val:  # Both nodes are bigger than the current value, search right
+            return helper(curr.right)
+        if curr.val > p.val and curr.val > q.val:  # Both nodes are smaller than the current value, search left
+            return helper(curr.left)
+        return curr  # Means current node is equal to one of the current nodes, this is where the split starts
+
+    return helper(root)
+
+
+'''Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).'''
+
+
+@time_execution()
+def level_order_traversal_iter(root: TreeNode) -> list[list[int]]:
+    res = []
+    if not root:
+        return res
+
+    queue = deque([root])
+    while queue:
+        curr_level = []
+        for _ in range(len(queue)):
+            curr = queue.popleft()
+            curr_level.append(curr.val)
+            if curr.left:
+                queue.append(curr.left)
+            if curr.right:
+                queue.append(curr.right)
+
+        res.append(curr_level)
+
+    return res
+
+
+@time_execution()
+def level_order_traversal_rec(root: TreeNode) -> list[list[int]]:
+    if not root:
+        return []
+    res = [[]]
+
+    def helper(node: TreeNode, depth: int) -> None:
+        if not node:
+            return
+
+        if depth < len(res):
+            res[depth].append(node.val)
+        else:
+            res.append([node.val])
+
+        helper(node.left, depth+1)
+        helper(node.right, depth+1)
+
+    helper(root, 0)
+    return res
